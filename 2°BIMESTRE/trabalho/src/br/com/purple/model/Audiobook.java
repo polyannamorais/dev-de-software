ebef08958f7d2536a13ef128db38c71f5457b23e
Polyanna Morais Barreto <100957130+polyannamorais@users.noreply.github.com>
2022-05-15 16:45:00 -0300

    Add files via upload

---
+++ 2°BIMESTRE/trabalho/src/br/com/purple/model/Audiobook.java
@@ -0,0 +1,7 @@
+package br.com.purple.model;
+
+public class Audiobook extends Livro {
+	
+	public String plataforma;
+
+}
---
+++ 2°BIMESTRE/trabalho/src/br/com/purple/model/Book.java
@@ -0,0 +1,5 @@
+package br.com.purple.model;
+
+public class Book extends Livro {
+
+}
